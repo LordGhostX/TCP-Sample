f13ea4a40c86556e7fe4b8c8e089acde2ba5fd29
LordGhostX <solomonghost9@gmail.com>
2019-09-18 03:37:49 +0000

    initial commit

---
+++ client.py
@@ -0,0 +1,31 @@
+# client.py
+
+import socket
+import sys
+
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect the socket to the port where the server is listening
+server_address = ('localhost', 10000)
+print('connecting to {} port {}'.format(*server_address))
+sock.connect(server_address)
+
+try:
+    while True:
+        # Send data
+        message = input("Enter message to send: ").encode()
+        sock.sendall(message)
+
+        # Look for the response
+        amount_received = 0
+        amount_expected = len(message)
+
+        while amount_received < amount_expected:
+            data = sock.recv(16)
+            amount_received += len(data)
+            print('received message {}'.format(data.decode()))
+
+finally:
+    print('closing socket')
+    sock.close()
